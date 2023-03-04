6331b0d2c9eafb9a381d48bbee125e42d56ca51d
zsigmondviktor99 <zsigmond.viktor.arpad@ruander.hu>
2023-03-04 14:11:45 +0100

    Error komponens felvele, oldalak minimalisan megformazva

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { FooldalComponent } from './fooldal/fooldal.component';
+import { KapcsolatComponent } from './kapcsolat/kapcsolat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "fooldal", component: FooldalComponent},
+  {path: "kapcsolat", component: KapcsolatComponent},
+  {path: "", pathMatch: "full", redirectTo: "fooldal"},
+  {path: "**", component: ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooldalComponent } from './fooldal/fooldal.component';
 import { KapcsolatComponent } from './kapcsolat/kapcsolat.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooldalComponent,
-    KapcsolatComponent
+    KapcsolatComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
